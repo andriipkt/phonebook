d0d9f70b68cdb673e8fc870dc32b4e3bc306d5df
andriipkt <spinnevski@ukr.net>
2023-11-02 08:48:00 -0700

    cmt

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -2,9 +2,21 @@ import { combineReducers } from 'redux';
 import { reducerContactsSlice } from './contacts/contactsSlice';
 import { reducerFilterSlice } from './filter/filterSlice';
 import { reducerAuthSlice } from './auth/authSlice';
+import storage from 'redux-persist/lib/storage';
+import persistReducer from 'redux-persist/es/persistReducer';
+
+const persistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
+const persistedReducerAuthSlice = persistReducer(
+  persistConfig,
+  reducerAuthSlice
+);
 
 export const rootReducer = combineReducers({
   contacts: reducerContactsSlice,
   filter: reducerFilterSlice,
-  authentication: reducerAuthSlice,
+  authentication: persistedReducerAuthSlice,
 });
